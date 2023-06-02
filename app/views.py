0e65378f619529d80c3dd2f3bf1b3408cb9ced85
Juan <111772146+Juan-Severiano@users.noreply.github.com>
2023-06-02 23:39:07 +0000

    quase completo

--- app/views.py
+++ app/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
+from .models import ItemList
+from datetime import datetime
 
-# Create your views here.
 
 def home(request):
-    return render(request, 'pages/home.html')
+    data_atual = datetime.today()
+    item_list = ItemList.objects.filter(due_date__lte=data_atual).order_by('-due_date')
+    item_list_today = ItemList.objects.filter(due_date=data_atual).order_by('-due_date')
+    item_list_past = ItemList.objects.filter(due_date__lt=data_atual).order_by('-due_date')
+    return render(request, 'pages/home.html', context={
+        'item_list': item_list,
+        'atual_date': data_atual,
+        'item_list_past':item_list_past,
+        'item_today': item_list_today,
+    })
