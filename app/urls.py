596a0ee97223ec16b428bacade0a0647c2e83272
Juan <111772146+Juan-Severiano@users.noreply.github.com>
2023-06-04 03:08:27 +0000

    colocando os links do navbar p funcionar

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home
+from .views import home, to_do
 
 urlpatterns = [
-    path('', home),
+    path('', home, name='home'),
+    path('to_do/', to_do, name='to_do')
 ]
--- app/views.py
+++ app/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render
 from .models import ItemList
 from datetime import datetime
 
+data_atual = datetime.today()
 
 def home(request):
-    data_atual = datetime.today()
     item_list = ItemList.objects.filter(due_date__gt=data_atual).order_by('due_date')
     item_list_today = ItemList.objects.filter(due_date=data_atual).order_by('-due_date')
     item_list_past = ItemList.objects.filter(due_date__lt=data_atual).order_by('-due_date')
@@ -13,4 +13,11 @@ def home(request):
         'atual_date': data_atual,
         'item_list_past':item_list_past,
         'item_today': item_list_today,
+    })
+
+
+def to_do(request):
+    item_list = ItemList.objects.filter(due_date__gt=data_atual).order_by('due_date')
+    return render(request, 'pages/to_do.html', context={
+        'item_to_do': item_list,
     })
