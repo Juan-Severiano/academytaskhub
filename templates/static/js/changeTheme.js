7c6fe1df04b186db817b5c96438dcc210f9c2fab
juan <juansoussev@gmail.com>
2023-07-03 18:24:20 -0300

    changeTheme concluido

--- templates/static/js/changeTheme.js
+++ templates/static/js/changeTheme.js
@@ -1,29 +1,29 @@
-const changeElement = document.querySelector('i#change-theme');
-const body = document.querySelector('body');
-let isDarkTheme = false;
+const changeElement = document.querySelector('i#change-theme')
+const body = document.querySelector('body')
+let isDarkTheme = false
 
 
 function toggleTheme() {
-  isDarkTheme = !isDarkTheme;
+  isDarkTheme = !isDarkTheme
   if (isDarkTheme) {
-    body.classList.add('dark-theme');
-    changeElement.classList.toggle('bi-sun')
-    localStorage.setItem('themePreference', 'dark');
+    body.classList.add('dark-theme')
+    changeElement.classList.replace('bi-moon-stars', 'bi-sun')
+    localStorage.setItem('themePreference', 'dark')
   } else {
-    body.classList.remove('dark-theme');
-    changeElement.classList.replace('bi-moon', 'bi-sun');
-    localStorage.setItem('themePreference', 'light');
+    body.classList.remove('dark-theme')
+    changeElement.classList.replace('bi-sun', 'bi-moon-stars')
+    localStorage.setItem('themePreference', 'light')
   }
 }
 
 
 
-const savedThemePreference = localStorage.getItem('themePreference');
+const savedThemePreference = localStorage.getItem('themePreference')
 if (savedThemePreference === 'dark') {
-  toggleTheme(); 
+  toggleTheme()
 }
 
 
 
 
-changeElement.addEventListener('click', toggleTheme);
+changeElement.addEventListener('click', toggleTheme)
